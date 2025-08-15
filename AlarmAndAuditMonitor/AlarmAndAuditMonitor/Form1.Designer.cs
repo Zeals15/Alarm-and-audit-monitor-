9c452ec03428b83e7ad2a17a206cd5a12ca986e7
Zeals15 <120047474+Zeals15@users.noreply.github.com>
2025-08-15 22:56:50 +0530

    Add files via upload

---
+++ AlarmAndAuditMonitor/AlarmAndAuditMonitor/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace AlarmAndAuditMonitor
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabAlarms;
+        private System.Windows.Forms.TabPage tabAudit;
+
+        private System.Windows.Forms.Panel panelAlarmHost;
+        private System.Windows.Forms.Panel panelAuditHost;
+
+        private System.Windows.Forms.FlowLayoutPanel barAlarms;
+        private System.Windows.Forms.Button btnRefreshAlarms;
+        private System.Windows.Forms.CheckBox chkAuto;
+        private System.Windows.Forms.NumericUpDown numSecs;
+        private System.Windows.Forms.Label lblSec;
+
+        private System.Windows.Forms.FlowLayoutPanel barAudit;
+        private System.Windows.Forms.Button btnRefreshAudit;
+
+        private System.Windows.Forms.DataGridView dgvAlarmServer;
+        private System.Windows.Forms.DataGridView dgvAuditLog;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabAlarms = new System.Windows.Forms.TabPage();
+            this.panelAlarmHost = new System.Windows.Forms.Panel();
+            this.dgvAlarmServer = new System.Windows.Forms.DataGridView();
+            this.barAlarms = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnRefreshAlarms = new System.Windows.Forms.Button();
+            this.chkAuto = new System.Windows.Forms.CheckBox();
+            this.numSecs = new System.Windows.Forms.NumericUpDown();
+            this.lblSec = new System.Windows.Forms.Label();
+            this.tabAudit = new System.Windows.Forms.TabPage();
+            this.panelAuditHost = new System.Windows.Forms.Panel();
+            this.dgvAuditLog = new System.Windows.Forms.DataGridView();
+            this.barAudit = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnRefreshAudit = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabAlarms.SuspendLayout();
+            this.panelAlarmHost.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAlarmServer)).BeginInit();
+            this.barAlarms.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numSecs)).BeginInit();
+            this.tabAudit.SuspendLayout();
+            this.panelAuditHost.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAuditLog)).BeginInit();
+            this.barAudit.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabAlarms);
+            this.tabControl1.Controls.Add(this.tabAudit);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1200, 650);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabAlarms
+            // 
+            this.tabAlarms.Controls.Add(this.panelAlarmHost);
+            this.tabAlarms.Location = new System.Drawing.Point(4, 25);
+            this.tabAlarms.Name = "tabAlarms";
+            this.tabAlarms.Size = new System.Drawing.Size(1192, 621);
+            this.tabAlarms.TabIndex = 0;
+            this.tabAlarms.Text = "AlarmLog";
+            // 
+            // panelAlarmHost
+            // 
+            this.panelAlarmHost.Controls.Add(this.dgvAlarmServer);
+            this.panelAlarmHost.Controls.Add(this.barAlarms);
+            this.panelAlarmHost.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelAlarmHost.Location = new System.Drawing.Point(0, 0);
+            this.panelAlarmHost.Name = "panelAlarmHost";
+            this.panelAlarmHost.Size = new System.Drawing.Size(1192, 621);
+            this.panelAlarmHost.TabIndex = 0;
+            // 
+            // dgvAlarmServer
+            // 
+            this.dgvAlarmServer.ColumnHeadersHeight = 29;
+            this.dgvAlarmServer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvAlarmServer.Location = new System.Drawing.Point(0, 36);
+            this.dgvAlarmServer.Name = "dgvAlarmServer";
+            this.dgvAlarmServer.RowHeadersWidth = 51;
+            this.dgvAlarmServer.Size = new System.Drawing.Size(1192, 585);
+            this.dgvAlarmServer.TabIndex = 0;
+            // 
+            // barAlarms
+            // 
+            this.barAlarms.Controls.Add(this.btnRefreshAlarms);
+            this.barAlarms.Controls.Add(this.chkAuto);
+            this.barAlarms.Controls.Add(this.numSecs);
+            this.barAlarms.Controls.Add(this.lblSec);
+            this.barAlarms.Dock = System.Windows.Forms.DockStyle.Top;
+            this.barAlarms.Location = new System.Drawing.Point(0, 0);
+            this.barAlarms.Name = "barAlarms";
+            this.barAlarms.Padding = new System.Windows.Forms.Padding(8, 6, 8, 6);
+            this.barAlarms.Size = new System.Drawing.Size(1192, 36);
+            this.barAlarms.TabIndex = 1;
+            // 
+            // btnRefreshAlarms
+            // 
+            this.btnRefreshAlarms.AutoSize = true;
+            this.btnRefreshAlarms.Location = new System.Drawing.Point(11, 9);
+            this.btnRefreshAlarms.Name = "btnRefreshAlarms";
+            this.btnRefreshAlarms.Size = new System.Drawing.Size(109, 26);
+            this.btnRefreshAlarms.TabIndex = 0;
+            this.btnRefreshAlarms.Text = "Refresh Alarms";
+            this.btnRefreshAlarms.Click += new System.EventHandler(this.btnRefreshAlarms_Click);
+            // 
+            // chkAuto
+            // 
+            this.chkAuto.AutoSize = true;
+            this.chkAuto.Location = new System.Drawing.Point(135, 12);
+            this.chkAuto.Margin = new System.Windows.Forms.Padding(12, 6, 6, 6);
+            this.chkAuto.Name = "chkAuto";
+            this.chkAuto.Size = new System.Drawing.Size(101, 20);
+            this.chkAuto.TabIndex = 1;
+            this.chkAuto.Text = "Auto-refresh";
+            this.chkAuto.CheckedChanged += new System.EventHandler(this.chkAuto_CheckedChanged);
+            // 
+            // numSecs
+            // 
+            this.numSecs.Location = new System.Drawing.Point(248, 9);
+            this.numSecs.Margin = new System.Windows.Forms.Padding(6, 3, 0, 3);
+            this.numSecs.Maximum = new decimal(new int[] {
+            3600,
+            0,
+            0,
+            0});
+            this.numSecs.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numSecs.Name = "numSecs";
+            this.numSecs.Size = new System.Drawing.Size(60, 22);
+            this.numSecs.TabIndex = 2;
+            this.numSecs.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numSecs.ValueChanged += new System.EventHandler(this.numSecs_ValueChanged);
+            // 
+            // lblSec
+            // 
+            this.lblSec.AutoSize = true;
+            this.lblSec.Location = new System.Drawing.Point(314, 14);
+            this.lblSec.Margin = new System.Windows.Forms.Padding(6, 8, 0, 0);
+            this.lblSec.Name = "lblSec";
+            this.lblSec.Size = new System.Drawing.Size(29, 16);
+            this.lblSec.TabIndex = 3;
+            this.lblSec.Text = "sec";
+            // 
+            // tabAudit
+            // 
+            this.tabAudit.Controls.Add(this.panelAuditHost);
+            this.tabAudit.Location = new System.Drawing.Point(4, 25);
+            this.tabAudit.Name = "tabAudit";
+            this.tabAudit.Size = new System.Drawing.Size(1192, 621);
+            this.tabAudit.TabIndex = 1;
+            this.tabAudit.Text = "AuditLog";
+            // 
+            // panelAuditHost
+            // 
+            this.panelAuditHost.Controls.Add(this.dgvAuditLog);
+            this.panelAuditHost.Controls.Add(this.barAudit);
+            this.panelAuditHost.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelAuditHost.Location = new System.Drawing.Point(0, 0);
+            this.panelAuditHost.Name = "panelAuditHost";
+            this.panelAuditHost.Size = new System.Drawing.Size(1192, 621);
+            this.panelAuditHost.TabIndex = 0;
+            // 
+            // dgvAuditLog
+            // 
+            this.dgvAuditLog.ColumnHeadersHeight = 29;
+            this.dgvAuditLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvAuditLog.Location = new System.Drawing.Point(0, 36);
+            this.dgvAuditLog.Name = "dgvAuditLog";
+            this.dgvAuditLog.RowHeadersWidth = 51;
+            this.dgvAuditLog.Size = new System.Drawing.Size(1192, 585);
+            this.dgvAuditLog.TabIndex = 0;
+            // 
+            // barAudit
+            // 
+            this.barAudit.Controls.Add(this.btnRefreshAudit);
+            this.barAudit.Dock = System.Windows.Forms.DockStyle.Top;
+            this.barAudit.Location = new System.Drawing.Point(0, 0);
+            this.barAudit.Name = "barAudit";
+            this.barAudit.Padding = new System.Windows.Forms.Padding(8, 6, 8, 6);
+            this.barAudit.Size = new System.Drawing.Size(1192, 36);
+            this.barAudit.TabIndex = 1;
+            // 
+            // btnRefreshAudit
+            // 
+            this.btnRefreshAudit.AutoSize = true;
+            this.btnRefreshAudit.Location = new System.Drawing.Point(11, 9);
+            this.btnRefreshAudit.Name = "btnRefreshAudit";
+            this.btnRefreshAudit.Size = new System.Drawing.Size(97, 26);
+            this.btnRefreshAudit.TabIndex = 0;
+            this.btnRefreshAudit.Text = "Refresh Audit";
+            this.btnRefreshAudit.Click += new System.EventHandler(this.btnRefreshAudit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 650);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Alarm / Audit Monitor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabAlarms.ResumeLayout(false);
+            this.panelAlarmHost.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAlarmServer)).EndInit();
+            this.barAlarms.ResumeLayout(false);
+            this.barAlarms.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numSecs)).EndInit();
+            this.tabAudit.ResumeLayout(false);
+            this.panelAuditHost.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAuditLog)).EndInit();
+            this.barAudit.ResumeLayout(false);
+            this.barAudit.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ AlarmAndAuditMonitor/AlarmAndAuditMonitor/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlarmAndAuditMonitor
+{
+    public partial class Form1 : Form
+    {
+        private readonly string _cs =
+            ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
+
+        private Timer _alarmTimer;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _alarmTimer = new Timer();
+            _alarmTimer.Interval = 5000;
+            _alarmTimer.Tick += async (s, e) => await LoadAlarmServerAsync();
+
+            PrepareGrid(dgvAlarmServer, true);
+            PrepareGrid(dgvAuditLog, true);
+        }
+
+        // ---------- grid look & header guarantees ----------
+        private void PrepareGrid(DataGridView g, bool readOnly)
+        {
+            g.ReadOnly = readOnly;
+            g.AllowUserToAddRows = false;
+            g.AllowUserToDeleteRows = false;
+            g.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            g.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            g.RowHeadersVisible = false;
+
+            g.ColumnHeadersVisible = true;
+            g.EnableHeadersVisualStyles = false;
+            g.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            g.ColumnHeadersHeight = 36;
+            g.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            g.ColumnHeadersDefaultCellStyle.BackColor = Color.SteelBlue;
+            g.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+            g.ColumnHeadersDefaultCellStyle.Font = new Font(g.Font, FontStyle.Bold);
+
+            g.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(245, 248, 255);
+        }
+
+        // ---------- form load ----------
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            await LoadAlarmServerAsync();
+            await LoadAuditLogAsync();
+        }
+
+        // ---------- UI events ----------
+        private async void btnRefreshAlarms_Click(object sender, EventArgs e)
+        {
+            await LoadAlarmServerAsync();
+        }
+
+        private async void btnRefreshAudit_Click(object sender, EventArgs e)
+        {
+            await LoadAuditLogAsync();
+        }
+
+        private void chkAuto_CheckedChanged(object sender, EventArgs e)
+        {
+            _alarmTimer.Enabled = chkAuto.Checked;
+        }
+
+        private void numSecs_ValueChanged(object sender, EventArgs e)
+        {
+            _alarmTimer.Interval = (int)numSecs.Value * 1000;
+        }
+
+        // ---------- build columns (explicit) ----------
+        private void BuildAlarmColumns()
+        {
+            var g = dgvAlarmServer;
+            g.AutoGenerateColumns = false;
+            g.Columns.Clear();
+
+            g.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "colA",
+                HeaderText = "Active Time",
+                DataPropertyName = "Active Time",
+                FillWeight = 25
+            });
+            g.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "colAck",
+                HeaderText = "Acknowledge Time",
+                DataPropertyName = "Acknowledge Time",
+                FillWeight = 25
+            });
+            g.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "colI",
+                HeaderText = "Inactive Time",
+                DataPropertyName = "Inactive Time",
+                FillWeight = 25
+            });
+            g.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "colText",
+                HeaderText = "Text",
+                DataPropertyName = "Text",
+                FillWeight = 25
+            });
+
+            g.Columns["colA"].DefaultCellStyle.Format   = "yyyy-MM-dd HH:mm:ss";
+            g.Columns["colAck"].DefaultCellStyle.Format = "yyyy-MM-dd HH:mm:ss";
+            g.Columns["colI"].DefaultCellStyle.Format   = "yyyy-MM-dd HH:mm:ss";
+        }
+
+        private void BuildAuditColumns()
+        {
+            var g = dgvAuditLog;
+            g.AutoGenerateColumns = false;
+            g.Columns.Clear();
+
+            g.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "colTime",
+                HeaderText = "Time",
+                DataPropertyName = "Time",
+                FillWeight = 20
+            });
+            g.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "colDesc",
+                HeaderText = "Description",
+                DataPropertyName = "Description",
+                FillWeight = 40
+            });
+            g.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "colAfter",
+                HeaderText = "Value After",
+                DataPropertyName = "Value After",
+                FillWeight = 13
+            });
+            g.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "colBefore",
+                HeaderText = "Value Before",
+                DataPropertyName = "Value Before",
+                FillWeight = 13
+            });
+            g.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "colUser",
+                HeaderText = "User Name",
+                DataPropertyName = "User Name",
+                FillWeight = 14
+            });
+
+            g.Columns["colTime"].DefaultCellStyle.Format = "yyyy-MM-dd HH:mm:ss";
+        }
+
+        // ---------- data loaders ----------
+        private async Task LoadAlarmServerAsync()
+        {
+            const string sql = @"
+SELECT TOP (1000)
+       [ActiveTime]       AS [Active Time],
+       [AcknowledgeTime]  AS [Acknowledge Time],
+       [InActiveTime]     AS [Inactive Time],
+       [Text]
+FROM [dbo].[AlarmServer]
+ORDER BY [AcknowledgeTime] DESC;";
+
+            try
+            {
+                using (var con = new SqlConnection(_cs))
+                using (var da = new SqlDataAdapter(sql, con))
+                {
+                    var dt = new DataTable();
+                    await con.OpenAsync();
+                    da.Fill(dt);
+
+                    BuildAlarmColumns();      // ensure columns exist BEFORE binding
+                    dgvAlarmServer.DataSource = dt;
+
+                    // force header visible (defensive)
+                    dgvAlarmServer.ColumnHeadersVisible = true;
+                    dgvAlarmServer.Invalidate();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Load Alarms Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async Task LoadAuditLogAsync()
+        {
+            const string sql = @"
+SELECT TOP (1000)
+       [TimeStamp]   AS [Time],
+       [Description],
+       [ValueAfter]  AS [Value After],
+       [ValueBefore] AS [Value Before],
+       [UserName]    AS [User Name]
+FROM [dbo].[AuditLog]
+ORDER BY [TimeStamp] DESC;";
+
+            try
+            {
+                using (var con = new SqlConnection(_cs))
+                using (var da = new SqlDataAdapter(sql, con))
+                {
+                    var dt = new DataTable();
+                    await con.OpenAsync();
+                    da.Fill(dt);
+
+                    BuildAuditColumns();     
+                    dgvAuditLog.DataSource = dt;
+
+                    dgvAuditLog.ColumnHeadersVisible = true;
+                    dgvAuditLog.Invalidate();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Load Audit Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        
+    }
+}
